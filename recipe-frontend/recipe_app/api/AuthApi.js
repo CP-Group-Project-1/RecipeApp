df206f4fefc1bd5167e35b9dc85eebdc60b58c92
TIZZDALE <TIZZDALE.FRAZIER@GMAIL.COM>
2025-03-18 13:09:22 -0700

    Updated authAPI.js with signup endpoint

--- recipe-frontend/recipe_app/api/AuthApi.js
+++ recipe-frontend/recipe_app/api/AuthApi.js
@@ -1,21 +1,21 @@
-// async function basicFetch(url, payload) {
-//     const res = await fetch(url, payload)
-//     const body = await res.json()
-//     return body
-//   }
+async function basicFetch(url, payload) {
+    const res = await fetch(url, payload)
+    const body = await res.json()
+    return body
+  }
 
 
-// export async function signup(context) {
-// const payload = {
-//     method: "POST",
-//     headers: {
-//     "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(context)
-// }
-// const body = await basicFetch("<endpoint>",payload)
-// return body
-// }
+export async function signup(context) {
+const payload = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    },
+    body: JSON.stringify(context)
+}
+const body = await basicFetch("http://127.0.0.1:8000/user_accounts/signup",payload)
+return body
+}
 
 
 // export async function login(context) {
