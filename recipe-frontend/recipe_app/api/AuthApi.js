6ccbbac38e41d469bdefb6157f8941a70fc8ef9c
TIZZDALE <TIZZDALE.FRAZIER@GMAIL.COM>
2025-03-17 19:28:34 -0700

    Added frontend api folder

---
+++ recipe-frontend/recipe_app/api/AuthApi.js
@@ -0,0 +1,32 @@
+// async function basicFetch(url, payload) {
+//     const res = await fetch(url, payload)
+//     const body = await res.json()
+//     return body
+//   }
+
+
+// export async function signup(context) {
+// const payload = {
+//     method: "POST",
+//     headers: {
+//     "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(context)
+// }
+// const body = await basicFetch("<endpoint>",payload)
+// return body
+// }
+
+
+// export async function login(context) {
+// const payload = {
+//     method: "POST",
+//     headers: {
+//     "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(context)
+// }
+// const body = await basicFetch("<endpoint>", payload)
+// return body.token
+// }
+
