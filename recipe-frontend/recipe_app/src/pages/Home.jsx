8939dabbf5367337edeffd3c19c92b792847fe5b
TIZZDALE <TIZZDALE.FRAZIER@GMAIL.COM>
2025-03-17 19:21:02 -0700

    Started Login Page and Home page

---
+++ recipe-frontend/recipe_app/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+    const navigate = useNavigate();
+    
+    return (
+        <>
+        <h2>Home Page</h2>
+
+        {/* Will remove once we create a NavBar */}
+        <button onClick={() => navigate('/login')}>
+            Login
+            </button> 
+        </>
+    )
+}
---
+++ recipe-frontend/recipe_app/src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+
+    const navigate = useNavigate();
+
+  return (
+    <>
+        <h2>Login</h2>
+        <br></br>
+        <form>
+          <div >
+            <label>Email</label>
+            <input
+              type="text"
+              name="username"
+              required
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              required
+            />
+          </div>
+          <div>
+            <button>Submit</button>
+            <button onClick={() => navigate('/')}>
+                Cancel
+            </button>
+          </div>
+        </form>
+    </>
+  );
+}
+
