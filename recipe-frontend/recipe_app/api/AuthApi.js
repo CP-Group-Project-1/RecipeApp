84788fcf4239e009c88f1ef814519c26b03eb482
mmccla1n <mcclainm1k3@gmail.com>
2025-03-22 15:21:50 -0500

    Updated backend to support storing the meal image in a field for saved_recipes..updated SavedRecipes.jsx as well

--- recipe-frontend/recipe_app/api/AuthApi.js
+++ recipe-frontend/recipe_app/api/AuthApi.js
@@ -82,3 +82,33 @@ export async function saveRecipe(userId, context) {
         return { success: false, error: error.message };
     }
 }
+
+export async function getRecipe(userId) {
+    //console.log('IN_getRecipe')
+    //console.log(`userId = ${userId}`)
+    //console.log(`Getting token from local storage => [${localStorage.getItem('token')}]`)
+  
+    const payload = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem('token')}`
+        },
+    };
+    //console.log(`payload = ${JSON.stringify(payload)}`)
+    try {
+        const body = await basicFetch(
+            `http://127.0.0.1:8000/saved_recipes/user/${userId}/`, 
+            payload
+        );
+        if (body) {
+            return { success: true, data: body };
+        } else {
+            return { success: false, error: body.error || "Failed to get recipe." };
+        }
+    } catch (error) {
+        console.error("Error getting recipe:", error);
+        return { success: false, error: error.message };
+    }
+}
+
