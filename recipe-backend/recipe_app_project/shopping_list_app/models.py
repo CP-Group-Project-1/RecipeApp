ba6431b907c1a32585a8c050b62133d470388443
Allison Crofton <illuminateddev@gmail.com>
2025-03-22 11:02:48 -0500

    updated shopping_list_app

---
+++ recipe-backend/recipe_app_project/shopping_list_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class ShoppingListItem(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.CharField(max_length=255)
+    qty = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True, default=1)
+
+    def __str__(self):
+        return f'{self.qty} {self.item} for {self.user.email}'
+
