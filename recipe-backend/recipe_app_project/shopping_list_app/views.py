e18eee2542d28bd33f1add8daf117c4cc641c8b4
Allison Crofton <illuminateddev@gmail.com>
2025-03-22 10:59:48 -0500

    added fraction handling in updating qty in shoppinglist

--- recipe-backend/recipe_app_project/shopping_list_app/views.py
+++ recipe-backend/recipe_app_project/shopping_list_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.db.models import F
+import re, math
+from fractions import Fraction
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,12 +8,33 @@ from rest_framework.permissions import IsAuthenticated
 from .models import ShoppingListItem
 from .serializers import ShoppingListItemSerializer
 
-# get all items on shopping list
+# get shopping list item by id
 def get_item(id):
     try:
         return ShoppingListItem.objects.get(id=id)
     except:
         return None
+
+# get quantity from Json and handle fractions
+def parse_quantity(measure_str):
+    if not measure_str:
+        return 1
+    
+    # match fractions and mixed fractions, or whole numbers
+    match = re.match(r"^\s*(\d+\s*\d+\/\d+|\d+\/\d+|\d+)",measure_str)
+    if match:
+        qty_str = match.group(1).strip()
+        try:
+            if ' ' in qty_str:
+                whole, fraction = qty_str.split()
+                qty = Fraction(int(whole)) + Fraction(fraction)
+            else:
+                qty = Fraction(qty_str)
+            
+            return math.ceil(float(qty))
+        except (ValueError, ZeroDivisionError):
+            return 1
+    return 1
 
 class ShoppingListItems(APIView):
     permission_classes = [IsAuthenticated]
@@ -30,14 +52,17 @@ class ShoppingListItems(APIView):
         for meal in meals_data:
             for i in range(1,21):
                 ingredient = meal.get(f"strIngredient{i}")
+                measure = meal.get(f"strMeasure{i}")
                 if ingredient and ingredient.strip():
                     ingredient = ingredient.strip()
-
+                    qty_to_add = parse_quantity(measure)
                     shopping_item, created = ShoppingListItem.objects.get_or_create(user=user, item=ingredient)
 
-                    if not created:
-                        shopping_item.qty = F('qty') + 1
-                        shopping_item.save()
+                    if created:
+                        shopping_item.qty = qty_to_add
+                    else:
+                        shopping_item.qty += qty_to_add
+                    shopping_item.save()
 
                     items.append(shopping_item)
         
@@ -63,12 +88,20 @@ class ShoppingListItemDetail(APIView):
     def patch(self, request, id):
         item = get_item(id)
         new_qty = request.data.get("qty")
+
+        try:
+            new_qty = float(new_qty)
+        except:
+            return Response({"error": "Quantity must be a number."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if new_qty <= 0:
+            return Response({"error": "Quantity must be greater than 0."}, status=status.HTTP_400_BAD_REQUEST)
         if item is None:
             return Response(f'Item does not exist.', status=status.HTTP_404_NOT_FOUND)
-        else:
-            item.qty = new_qty
-            item.save()
-            return Response({"message": "Item updated."}, status=status.HTTP_200_OK)
+        
+        item.qty = new_qty
+        item.save()
+        return Response({"message": "Item updated."}, status=status.HTTP_200_OK)
             
 
     def delete(self, request, id):
